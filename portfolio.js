e480be56095d3a1e91dfa3c49615b38c48297a06
YapSeng98 <77666588+YapSeng98@users.noreply.github.com>
2023-05-10 00:41:58 +0800

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,23 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    var scrollToTopButton = document.getElementById("scrollToTopButton");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopButton.style.display = "block";
-    } else {
-        scrollToTopButton.style.display = "none";
-    }
+  var scrollToTopButton = document.getElementById("scrollToTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
 }
 
 function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 function toggleNav() {
-    var navCollapse = document.getElementById("navbar-collapse");
-    if (navCollapse.classList.contains("open")) {
-        navCollapse.classList.remove("open");
-    } else {
-        navCollapse.classList.add("open");
-    }
+  var navCollapse = document.getElementById("navbar-collapse");
+  if (navCollapse.classList.contains("open")) {
+    navCollapse.classList.remove("open");
+  } else {
+    navCollapse.classList.add("open");
+  }
 }
