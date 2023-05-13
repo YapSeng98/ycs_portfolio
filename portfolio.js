1db12150218093a41340ac4704c39fbc734469f4
YapSeng98 <77666588+YapSeng98@users.noreply.github.com>
2023-05-13 12:58:39 +0800

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -21,3 +21,22 @@ function toggleNav() {
     navCollapse.classList.add("open");
   }
 }
+function openPowerappsSkill() {
+  window.location.href = "powerApps.html";
+}
+function openServiceNowSkill() {
+  window.location.href = "serviceNow.html";
+}
+function sendEmail() {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const from = document.getElementById("from");
+    from.value = email;
+    const body = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
+    window.location.href = `mailto:ycseng0398@gmail.com?subject=New message from ${name}&body=${encodeURIComponent(body)}`;
+  });
+}
